d484e57118a0499493a97d0503e5df478448bb38
CeciliaB22 <ceciliabalaban11@gmail.com>
2024-04-25 11:13:45 +0200

    petites modificacions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const date = document.querySelector('#date');
-const totalTasks = document.querySelector('#total-tasks');
-const completedTasks = document.querySelector('#completed-tasks');
+const total = document.querySelector('#total-tasks');
+const completed = document.querySelector('#complete-task');
 const addButton = document.querySelector('#add-task');
 const inputTask = document.querySelector('#input-task');
 const listTasks = document.querySelector('#list-tasks');
@@ -11,10 +11,10 @@ let id = 0;
 
 //para obtener la fecha actual
 const today = new Date();
-date.innerHTML = today.toLocaleDateString ('en', {weekday : 'long', month:'long', day:'numeric'});
+date.innerHTML = today.toLocaleDateString ('en-UK', {weekday : 'long', day:'numeric', month:'long', year:'numeric'});
 
 //creamos la función para agregar las tareas a la lista
-function addTaskFunction (task,id,done,deleted) {
+function addTaskFunction (task,id,done) {
     const DONE = done ? check : uncheck; //si la tarea esta completada entonces DONE es igual a check si no es igual a uncheck
     const LINE = done ? lineThrough : ''; //si la tarea esta completada entonces LINE es igual a line-through si no es igual a un string vacio
     //creamos el elemento que se va a agregar a la lista 
@@ -30,9 +30,8 @@ function addTaskFunction (task,id,done,deleted) {
 //agregamos el evento click al botón addTask
 addButton.addEventListener('click', () => {
     const task = inputTask.value //obtenemos el valor del input y lo asignamos a la variable task
-    //si la tarea existe entonce la agregamos 
     if (task) {
-      addTaskFunction(task, id, false, false) 
+      addTaskFunction(task, id, false) //
     }
     inputTask.value =''
     id++
@@ -64,3 +63,6 @@ function removeTask(element) {
     element.parentNode.parentNode.removeChild(element.parentNode);
     LIST[element.id].deleted = true;
 }
+
+
+
